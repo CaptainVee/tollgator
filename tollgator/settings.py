9a633bef0b924764a9cf55c015d3380e7415ddd5
unknown <captainvee3@gmail.com>
2021-01-28 00:18:00 +0100

    req4

--- tollgator/settings.py
+++ tollgator/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY2')
+SECRET_KEY = ')=lq-8v(m_kz704_b4-@i&!ic%98%ug0(ih71snm!btxu_4=bp)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
