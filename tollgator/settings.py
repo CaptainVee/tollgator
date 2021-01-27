18579cb598ba7f3729265e8dfbdc876466e94fd4
unknown <captainvee3@gmail.com>
2021-01-28 00:15:37 +0100

    req3

--- tollgator/settings.py
+++ tollgator/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY2')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tollgatorapp.herokuapp.com']
 
 
 # Application definition
