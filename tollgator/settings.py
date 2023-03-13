802f855645d3fc6281c4b6a2b61ff755102c154c
CaptainVee <captainvee3@gmail.com>
2023-03-13 17:52:14 +0100

    feat: changed some settings

--- tollgator/settings.py
+++ tollgator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "mysecretkey")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = True
 
 # ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
 ALLOWED_HOSTS = [
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = [
     "http://127.0.0.1:8000/",
     "localhost",
     "tollgator.com",
-    "https://tollgator.com",
-    "https://www.tollgator.com",
+    "tollgator.com",
+    "www.tollgator.com",
 ]
 
 
