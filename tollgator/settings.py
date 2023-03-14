909f029e142deb1f35e4acc2b23d4c62cb6ba5dd
CaptainVee <captainvee3@gmail.com>
2023-03-14 21:49:04 +0100

    feat: changes static url

--- tollgator/settings.py
+++ tollgator/settings.py
@@ -179,7 +179,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
