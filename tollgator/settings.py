3e66979011d0b1d52c6dc3d4e0cf7d29bae3a49c
CaptainVee <captainvee3@gmail.com>
2023-04-17 21:29:02 +0100

    chore: collected static files

--- tollgator/settings.py
+++ tollgator/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = get_secret("DEBUG")
+DEBUG = False
 
 ALLOWED_HOSTS = str(get_secret("ALLOWED_HOSTS")).split()
 
